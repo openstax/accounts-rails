9e955d6681f8ab85dd20b33440e1d785df29c890
Dante Soares <dms3@rice.edu>
2014-09-03 13:47:57 -0500

    Better factories

--- spec/factories/openstax_accounts_account.rb
+++ spec/factories/openstax_accounts_account.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :openstax_accounts_account, :class => OpenStax::Accounts::Account do
-    sequence(:openstax_uid) { -SecureRandom.hex.to_i(16) }
-    sequence(:username) { SecureRandom.hex }
+    openstax_uid { -SecureRandom.hex.to_i(16) }
+    username     { SecureRandom.hex }
+    access_token { SecureRandom.hex }
   end
 end
--- spec/factories/openstax_accounts_group.rb
+++ spec/factories/openstax_accounts_group.rb
@@ -2,10 +2,8 @@
 
 FactoryGirl.define do
   factory :openstax_accounts_group, :class => OpenStax::Accounts::Group do
-    sequence(:openstax_uid) { -SecureRandom.hex.to_i(16) }
+    openstax_uid { -SecureRandom.hex.to_i(16) }
     name "MyGroup"
     is_public false
-    cached_subtree_group_ids nil
-    cached_supertree_group_ids nil
   end
 end
