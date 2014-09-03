87820b30bf8097afc14fef96ca52ca503182fa0a
Dante Soares <dms3@rice.edu>
2014-09-03 14:21:04 -0500

    Explicitly transform binary data into strings in account factory

--- spec/factories/openstax_accounts_account.rb
+++ spec/factories/openstax_accounts_account.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :openstax_accounts_account, :class => OpenStax::Accounts::Account do
     openstax_uid { -SecureRandom.hex.to_i(16) }
-    username     { SecureRandom.hex }
-    access_token { SecureRandom.hex }
+    username     { SecureRandom.hex.to_s }
+    access_token { SecureRandom.hex.to_s }
   end
 end
