85a862cafe80db26045f3c6fb91ffceab8e571a6
Dante Soares <dms3@rice.edu>
2014-09-23 16:11:24 -0500

    Changed migration numbers to consecutive integers

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140811182553) do
+ActiveRecord::Schema.define(version: 6) do
 
   create_table "openstax_accounts_accounts", force: true do |t|
     t.integer  "openstax_uid", null: false
