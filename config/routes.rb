13c4df4edd3f2c57004922dc681647da6b4bc18f
JP Slavinsky <jpslav@gmail.com>
2014-10-14 16:15:06 -0700

    fixes rails deprecation warning about :to in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 OpenStax::Accounts::Engine.routes.draw do
 
   # Redirect here if we don't know what to do (theoretically should not happen)
-  root :to => 'sessions#new'
+  root controller: 'sessions', action: 'new'
 
   # Shortcut to OmniAuth route that redirects to the Accounts server
   # This is provided by OmniAuth and is not in the SessionsController
@@ -12,8 +12,8 @@ OpenStax::Accounts::Engine.routes.draw do
     get 'callback', :path => 'auth/:provider/callback' # Authentication success
     get 'failure', :path => 'auth/failure' # Authentication failure
 
-    get 'login', :to => :new # Redirects to /auth/openstax or stub
-    match 'logout', :to => :destroy, # Redirects to logout path or stub
+    get 'login', :action => :new # Redirects to /auth/openstax or stub
+    match 'logout', :action => :destroy, # Redirects to logout path or stub
                     :via => OpenStax::Accounts.configuration.logout_via
   end
 
