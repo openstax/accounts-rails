150ee70be9b3ff27a7532a5f7338177325ba007b
Dante Soares <dms3@rice.edu>
2014-10-14 10:00:33 -0500

    More conversion to new search routine

--- app/handlers/openstax/accounts/accounts_search.rb
+++ app/handlers/openstax/accounts/accounts_search.rb
@@ -4,7 +4,7 @@ module OpenStax
 
       lev_handler transaction: :no_transaction
 
-      uses_routine OpenStax::Utilities::SearchHandler,
+      uses_routine OpenStax::Utilities::KeywordSearchHandler,
                    as: :search_handler,
                    translations: { outputs: {type: :verbatim} }
 
@@ -16,10 +16,9 @@ module OpenStax
 
       def handle
         config = OpenStax::Accounts.configuration
-        run(:search_handler, query, search_routine: OpenStax::Accounts::SearchAccounts,
-                                    search_relation: OpenStax::Accounts::Account.unscoped,
-                                    min_characters: config.min_search_characters,
-                                    max_items: config.max_search_items)
+        run(:search_handler, params: params,
+                             search_routine: OpenStax::Accounts::SearchAccounts,
+                             search_relation: OpenStax::Accounts::Account.unscoped)
       end
 
     end
