b2c1e998b79ca988cb41183ccd8ae40ae579117d
Dante Soares <dms3@rice.edu>
2015-04-14 17:28:12 -0500

    Added routine to create temp users

---
+++ app/routines/openstax/accounts/create_temp_account.rb
@@ -0,0 +1,36 @@
+# Routine for creating a temporary account by email or username
+
+module OpenStax
+  module Accounts
+    class CreateTempAccount
+
+      lev_routine
+            
+      protected
+
+      def exec(inputs={})
+        result = Api.create_temp_account(inputs)
+        fatal_error(code: :invalid_inputs) unless result.success?
+
+        struct = OpenStruct.new
+        Api::V1::UnclaimedAccountRepresenter.new(struct).from_json(result)
+        id = struct.id
+
+        account = Account.find_or_initialize_by(openstax_uid: id)
+
+        unless account.persisted?
+          username = inputs[:username]
+          while username.nil? || Account.where(username: username).exists? do 
+            username = SecureRandom.hex(3).to_s
+          end
+          account.username = username
+          account.save!
+        end
+
+        transfer_errors_from(account, {type: :verbatim}, true)
+        outputs[:account] = account
+      end
+
+    end
+  end
+end
