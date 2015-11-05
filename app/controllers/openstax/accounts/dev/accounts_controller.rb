ceee47a84c1b844a1f9ef2d9c7425fcdcba24c01
Nathan Stitt <nathan@stitt.org>
2015-11-05 14:26:42 -0600

    Enable loading dev stub authentication from iframe

--- app/controllers/openstax/accounts/dev/accounts_controller.rb
+++ app/controllers/openstax/accounts/dev/accounts_controller.rb
@@ -2,6 +2,8 @@ module OpenStax
   module Accounts
     module Dev
       class AccountsController < OpenStax::Accounts::Dev::BaseController
+        # Allow accessing from inside an iframe
+        before_filter :allow_iframe_access, only: [:index, :search]
 
         def index
         end
@@ -19,6 +21,12 @@ module OpenStax
           @account = Account.find_by(openstax_uid: params[:id])
           sign_in(@account)
           redirect_back key: :accounts_return_to, strategies: [:session]
+        end
+
+        private
+
+        def allow_iframe_access
+          response.headers.except! 'X-Frame-Options'
         end
 
       end
