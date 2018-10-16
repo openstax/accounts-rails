702a9baaffca980964006dc76be3a53ded1858ce
Dante Soares <dante.m.soares@rice.edu>
2018-10-16 12:33:45 -0500

    Removed FactoryBot deprecation warnings

--- spec/factories/openstax_accounts_group.rb
+++ spec/factories/openstax_accounts_group.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
-  factory :openstax_accounts_group, :class => OpenStax::Accounts::Group do
+  factory :openstax_accounts_group, class: OpenStax::Accounts::Group do
     openstax_uid { -SecureRandom.hex(4).to_i(16)/2 }
-    name "MyGroup"
-    is_public false
+    name         { "MyGroup" }
+    is_public    { false }
   end
 end
--- spec/factories/openstax_accounts_group_owner.rb
+++ spec/factories/openstax_accounts_group_owner.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :openstax_accounts_group_owner, :class => OpenStax::Accounts::GroupOwner do
+  factory :openstax_accounts_group_owner, class: OpenStax::Accounts::GroupOwner do
     association :group, factory: :openstax_accounts_group
     association :user, factory: :openstax_accounts_account
   end
