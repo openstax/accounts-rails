4e4a9147e310c54e8722c3b6ad48754e81985614
Chris Bendel <Chris Bendel>
2024-03-13 11:22:14 -0400

    Remove references to sprockets, not included in rails anymore

--- spec/dummy/config/environments/development.rb
+++ spec/dummy/config/environments/development.rb
@@ -27,11 +27,6 @@ Dummy::Application.configure do
   # number of complex assets.
   # config.assets.debug = true
 
-  # Adds additional error checking when serving assets at runtime.
-  # Checks for improperly declared sprockets dependencies.
-  # Raises helpful error messages.
-  # config.assets.raise_runtime_errors = true
-
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
